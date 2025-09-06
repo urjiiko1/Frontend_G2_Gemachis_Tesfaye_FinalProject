d9f4b9f1ceeb91cfdeb6eeb246c88f5479ff8864
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-09-06 20:37:05 +0300

    fetch: add new code for open in default port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,  // Automatically open the app in the browser on server start
+    port: 3000,  // Specify the port number
+  },
 })
