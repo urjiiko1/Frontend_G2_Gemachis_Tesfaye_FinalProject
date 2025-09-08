1780a9ab0e91b1ce74cfd18619b823dfd80845f8
Gemachis Tesfaye <gemachistesfaye36@gmail.com>
2025-09-08 15:31:03 +0000

    Fixing Tailwind CSS and rendering issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import FlightsPage from './pages/FlightsPage';
+import React from "react";
+import FlightsPage from "./pages/FlightsPage";
 
 function App() {
   return (
     <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-      <script src="https://cdn.tailwindcss.com"></script>
       <FlightsPage />
     </>
   );
